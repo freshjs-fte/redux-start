183145ad169f398f9bc7d8a6d34d27287032d391
Yevhenii <freshjs.fte@gmail.com>
2021-10-21 21:12:47 +0300

    Add api calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,11 @@
+import axios from "axios"
 
-export const addTaskAsync = async () => {
-    // fetch
+export const getTasksAsync = async () => {
+    const response = await axios.get("http://192.168.1.148:5000/api/tasks")
+    return response.data.data
+}
+
+export const addTaskAsync = async (data) => {
+    const response = await axios.post("http://192.168.1.148:5000/api/tasks", data)
+    return response.data.data
 }
